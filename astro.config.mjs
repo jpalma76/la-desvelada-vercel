0166327839294e0d4fe85b8e389a6792a3bfc737
Juan Palma <juan.palma.alvarez@gmail.com>
2025-07-18 14:00:41 -0400

    prueba cssCodeSplit en false

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,17 +12,10 @@ export default defineConfig({
     plugins: [tailwindcss()],
 
     build: {
-      assetsInlineLimit: 0,
-      rollupOptions: {
-        output: {
-          entryFileNames: `assets/[name].[hash].js`,
-          chunkFileNames: `assets/[name].[hash].js`,
-          assetFileNames: `assets/[name].[hash].[ext]`,
-        },
-      },
+      cssCodeSplit: false // para evitar que el CSS se divida en muchos archivos
     },
   },
 
   adapter: vercel(),
-  
+
 });
