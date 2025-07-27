5111a6b8cc2386344963fa8b6b1e36a725fae0fe
Juan Palma <juan.palma.alvarez@gmail.com>
2025-07-26 20:48:00 -0400

    udate files

---
+++ src/consts/combats.ts
@@ -0,0 +1,37 @@
+export const COMBATS = [
+    {
+        id: 1,
+        fighter1: "peereira",
+        fighter2: "rivaldios"
+    },
+    {
+        id: 2,
+        fighter1: "perxitaa",
+        fighter2: "gaspi"
+    },
+    {
+        id: 3,
+        fighter1: "abby",
+        fighter2: "roro"
+    },
+    {
+        id: 4,
+        fighter1: "andoni",
+        fighter2: "carlos"
+    },
+    {
+        id: 5,
+        fighter1: "viruzz",
+        fighter2: "tomas"
+    },
+    {
+        id: 6,
+        fighter1: "alana",
+        fighter2: "arigeli"
+    },
+    {
+        id: 7,
+        fighter1: "grefg",
+        fighter2: "westcol"
+    }
+]
